961a248a913a88e570c3f4f03114cab432b94a6e
Rodion Basmanov <st135699@student.spbu.ru>
2025-05-15 04:28:46 +0300

    Implement Shannon-Fano decoder

---
+++ scripts/build_decoder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create build directory if it doesn't exist
+mkdir -p build
+
+# Compile the decoder
+g++ -std=c++11 -o build/decoder \
+    src/app/decoder_entry.cpp \
+    src/core/shannon_logic.cpp \
+    src/data_structures/frequency_map.cpp \
+    src/io/file_handler.cpp \
+    -I./src
+
+echo "Decoder compiled successfully. Binary is located at build/decoder" 
